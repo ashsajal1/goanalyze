bf1fc3e6c8785d673dbe467fa98f8017e2f18837
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-03-29 18:45:39 +0800

    Add link ro charts in button

--- src/components/analysis-card.tsx
+++ src/components/analysis-card.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from './ui/card'
 import Image from 'next/image'
 import { Button } from './ui/button'
+import Link from 'next/link'
 
 export default function AnalysisCard() {
   const img = 'https://www.tradingview.com/x/8GbwaRJX'
@@ -16,9 +17,11 @@ export default function AnalysisCard() {
         <img src={'https://www.tradingview.com/x/8GbwaRJX'} alt="fsa" />
         <CardDescription>This is the description of this analysis!</CardDescription>
       </CardContent>
-      <CardFooter className="flex justify-between">
-        <Button variant="outline">Cancel</Button>
-        <Button>Deploy</Button>
+      <CardFooter className="flex justify-between items-center gap-2">
+        <Link className='w-full' target='_blink' href={img}>
+          <Button className='w-full' variant="outline">View Charts</Button>
+          </Link>
+        <Button>See Deatils</Button>
       </CardFooter>
 
     </Card>
