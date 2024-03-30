cc688a06b42c9a2445dca8882a60024bde9d8924
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-03-30 10:34:05 +0800

    Redesign card

--- src/components/analysis-card.tsx
+++ src/components/analysis-card.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from './ui/card'
-import Image from 'next/image'
 import { Button } from './ui/button'
 import Link from 'next/link'
 
@@ -9,12 +8,12 @@ export default function AnalysisCard({ img }: { img: string }) {
     <Card className='md:w-[330px]'>
       <CardHeader>
         <CardTitle>Analysis of XECUSDT</CardTitle>
+        <CardDescription>This is the description of this analysis!</CardDescription>
       </CardHeader>
       <CardContent>
 
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img src={'https://www.tradingview.com/x/8GbwaRJX'} alt="fsa" />
-        <CardDescription>This is the description of this analysis!</CardDescription>
       </CardContent>
       <CardFooter className="flex justify-between items-center gap-2">
         <Link className='w-full' target='_blink' href={img}>
