06100d5b440dc55e96fea142280a67cf3a2f197b
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-03-30 17:41:25 +0600

    Create route handler with form

--- src/app/api/analysis/route.ts
+++ src/app/api/analysis/route.ts
@@ -1,6 +1,20 @@
 import { type NextRequest } from "next/server";
+import prisma from "../../../../prisma/client";
 
 export async function POST(request: NextRequest) {
   const res = await request.json();
-  return Response.json({ res });
+  try {
+    // Create the analysis using the form values
+    const createdAnalysis = await prisma.analysis.create({
+      data: {
+        imageUrl: res.imageurl,
+        description: res.description,
+        pair: res.pair,
+      },
+    });
+    return Response.json({ res } ,{status: 201});
+  } catch (error) {
+    return Response.json({ error } ,{status: 500});
+  }
+  
 }
