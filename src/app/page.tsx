79c830d1678f244f2751240af7426a27600f97fd
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-04-02 11:56:27 +0600

    Render data from api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,39 @@
+"use client"
+import { useState, useEffect } from 'react';
 import AnalysisCard from "@/components/analysis-card";
 
-export default async function Home() {
+export default function Home() {
+  const [analysisData, setAnalysisData] = useState([]);
+
+  useEffect(() => {
+    async function fetchAnalysisData() {
+      try {
+        const response = await fetch('/api/analysis');
+        if (!response.ok) {
+          throw new Error('Failed to fetch data');
+        }
+        const data = await response.json();
+        setAnalysisData(data.res);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    fetchAnalysisData();
+  }, []);
+
   return (
-    <main className="flex items-center justify-between flex-wrap gap-2 p-2 md:p-4">
-      <AnalysisCard img='https://www.tradingview.com/x/8GbwaRJX' />
-      <AnalysisCard img='https://www.tradingview.com/x/8GbwaRJX' />
-      <AnalysisCard img='https://www.tradingview.com/x/8GbwaRJX' />
-      <AnalysisCard img='https://www.tradingview.com/x/8GbwaRJX' />
-      <AnalysisCard img='https://www.tradingview.com/x/8GbwaRJX' />
-      <AnalysisCard img='https://www.tradingview.com/x/8GbwaRJX' />
-      <AnalysisCard img='https://www.tradingview.com/x/8GbwaRJX' />
-      <AnalysisCard img='https://www.tradingview.com/x/8GbwaRJX' />
-      <AnalysisCard img='https://www.tradingview.com/x/8GbwaRJX' />
+    <main>
+      {analysisData.length > 0 ? (
+        <div className="flex items-center justify-between flex-wrap gap-2 p-2 md:p-4">
+          {analysisData.map((item, index) => (
+            <AnalysisCard key={index} img={item.imageUrl} />
+          ))}
+        </div>
+      ) : (
+        <p>Loading analysis data...</p>
+      )}
     </main>
-  )
+  );
+  
 }
